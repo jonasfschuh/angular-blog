83273bbbb75443a3ef90f7ff368009cd53b487b5
kenzor1979 <jonasschuh@hotmail.com>
2024-08-31 16:12:01 -0300

    readme adjustments

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,20 @@
 export const dataFake = [
     {
       "id":"1",
-      "title": "NOVO HOMEM DE FERRO EM 3D",
-      "description": "marvel anuncia um novo filme do homem de ferro, confira",
+      "title": "NEW IRON MAN IN 3D",
+      "description": "Marvel studio announces new Iron Man movie. Marvel Studios returned to Hall H Saturday at San Diego Comic-Con (SDCC)",
       "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
     },
     {
       "id":"2",
-      "title": "Nova Série anunciada no Disney +",
-      "description": "bla blabla",
+      "title": "New Series Announced on Disney+",
+      "description": "Marvel Studios returned to Hall H Saturday at San Diego Comic-Con (SDCC), giving fans a first look at what’s " + 
+       " next for the Marvel Cinematic Universe (MCU)—including huge news about Marvel Studios’ next two Avengers films, " + 
+       " including the return of Disney Legend Robert Downey Jr. in a new role as Doctor Doom. " + 
+
+      " Flanked by others dressed as Doctor Doom, Downey unmasked himself onstage at the end of the presentation. " +
+      " “New mask, same task,” said the Academy Award® winner, last seen in the MCU as Tony Stark/Iron Man. " +
+      " “What can I tell you? I like playing complicated character.”", 
       "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
     }
   ]
